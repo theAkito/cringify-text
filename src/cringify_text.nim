b20f4c1cd420fe5f1a117e5a0ca56e899b154ed8
Akito <akito.kitsune@protonmail.com>
2019-09-19 01:05:39 +0200

    Done 1. But Unicode characters are replaced by whitespace. 2. And if the string has an odd amount of characters, it may cut off the last character. This may be a positive effect, though, to improve cringiness.

--- src/cringify_text.nim
+++ src/cringify_text.nim
@@ -1,4 +1,34 @@
-import
-  strutils,
-  unicode,
-  clipper
+import clipper
+from unicode import add, runeAt, runeLen, toUpper
+from strutils import multireplace
+
+proc getText(): string =
+  let text: string = stdin.readLine
+  case text
+  of "", "\n", " \n":
+    "Text cannot be empty.".echo
+  else:
+    return text
+
+proc cringify() =
+  let
+    inputText: string = getText()
+  var
+    iter = 0
+    itimStd = 0
+    itimUp = 1
+    cringifiedText: string
+  while iter <= inputText.runeLen:
+    cringifiedText.add(inputText.runeAt(itimStd))
+    cringifiedText.add(inputText.runeAt(itimUp).toUpper)
+    itimStd += 2
+    itimUp += 2
+    iter.inc
+    if itimUp >= inputText.runeLen:
+      break
+    elif itimStd >= inputText.runeLen:
+      break
+  cringifiedText = cringifiedText.multireplace(replacements = ("i", "ii"), ("I", "ii"))
+  discard cringifiedText.clip()
+
+cringify()
